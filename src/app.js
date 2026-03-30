55dd89658c2313d3240e5ac0e2a046aebf49bd43
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-03-30 11:32:29 +0500

    add EJS view engine support; create layout and partials for header and footer; add home page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
-const express = require("express")
+const express = require('express');
+const path = require('path');
+const webRoutes = require('./routes/web');
 
 const app = express();
 
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Static files
+app.use(express.static(path.join(__dirname, '../public')));
 
+// Routes
+app.use('/', webRoutes);
 
-module.exports = app 
+module.exports = app;
